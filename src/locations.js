5ecfeb700d949243e62f9b7acfc4d2fee8c0f868
Kumush <kumushhon.rustamova@gmail.com>
2025-03-26 15:49:44 +0500

    Add video asset, update Location class with website property, and enhance HTML structure and CSS styles

--- src/locations.js
+++ src/locations.js
@@ -8,6 +8,7 @@ class Location {
     this.lat = data.lat;
     this.lng = data.lng;
     this.photo = data.photo;
+    this.website = data.website;
   }
 
   render() {
@@ -39,6 +40,12 @@ class Location {
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
                 </svg>
                 <span class="text-gray-600">${this.phone}</span>
+                </div>
+                 <div class="flex items-center">
+                <svg class="w-4 h-4 text-gray-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"/>
+                </svg>
+                <a href="${this.website}" class="text-gray-600">${this.website}</a>
                 </div>
             </div>
 
